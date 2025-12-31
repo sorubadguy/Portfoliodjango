66f47e681523b0327b20ee37fbf03ac0488e6699
Lucas <lucasmartinez33@gmail.com>
2025-12-31 00:57:40 -0300

    Add projects app with views, templates, and URL routing

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/project_list.html', {
+        'projects': projects
+    })
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'projects/project_detail.html', {
+        'project': project
+    })
